ff1f01004f8c3c6db68596d659093c785e810ce6
Lawrence Li <deja2011@163.com>
2016-11-20 21:45:20 +0800

    Added python 3 support to vim install script.

--- vim.install.sh
+++ vim.install.sh
@@ -5,6 +5,8 @@
     --enable-rubyinterp=yes \
     --enable-pythoninterp=yes\
     --with-python-config-dir=/usr/lib/python2.7/config \
+    --enable-python3interp=yes\
+    --with-python3-config-dir=/usr/lib/python3.5/config \
     --enable-perlinterp=yes \
     --enable-luainterp=yes \
     --enable-cscope \
